3469160c872b3c8d0cc6b5ed0dcff97265a85610
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2021-12-19 10:29:19 +0900

    [ADD] Logging 추가

--- src/typeorm/ormconfig.ts
+++ src/typeorm/ormconfig.ts
@@ -14,8 +14,9 @@ const connectionOptions: DBConnectionOptions = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: false,
     entities: [UploadImage],
+    synchronize: false,
+    logging: true,
   },
 };
 
