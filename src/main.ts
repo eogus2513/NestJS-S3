7c83f9cb45601bd61d89ef29d710a5bfaeb95ba1
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2021-12-21 10:36:38 +0900

    [FIX] test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 8000;
+  await app.listen(PORT);
+  console.log('server start! | ' + PORT);
 }
 
 bootstrap();
